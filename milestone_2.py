8f5f7e28e2cc573b8c977b7c0e6f54fc5fdbd1e7
shima-maleki <shimamaleki95@yahoo.com>
2024-05-24 17:51:42 +0100

    refactoring code

--- milestone_2.py
+++ milestone_2.py
@@ -1,16 +1,16 @@
 
 import random
-word_list=['apple', 'banana', 'orange', 'kiwi', 'strawberry']
+fruits_list=['apple', 'banana', 'orange', 'kiwi', 'strawberry']
 
-print(word_list)
+print(fruits_list)
 
 
-word= random.choice(word_list)      #random is module(package) and choice is function
+word= random.choice(fruits_list)      #random is module(package) and choice is function
 
 print(word)
 
-guess = input('enter the guess')
-if len(guess)== 1 and guess.isalpha():
+user_guess = input('enter the guess')
+if len(user_guess)== 1 and user_guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")    
