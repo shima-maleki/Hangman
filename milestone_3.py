f01665d020e515770f6b0b98b26481fe211387a9
shima-maleki <shimamaleki95@yahoo.com>
2024-05-24 18:26:31 +0100

    mile stone 3 refactor code

--- milestone_3.py
+++ milestone_3.py
@@ -1,20 +1,27 @@
 
 import random
+
 fruits_list=['apple', 'banana', 'orange', 'kiwi', 'strawberry']
 
 word= random.choice(fruits_list)      #random is module(package) and choice is function
 
-while True:
-    guess = input('guess a letter: ')
-    if guess.isalpha():
-        break
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f'Good guess! {guess} is in the word.')
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.") 
+
+def ask_for_input(): 
+    while True:
+        guess = input('guess a letter: ')
+        if guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.") 
+            
+    check_guess(guess) 
 
 
-if guess in word:
-    print(f'Good guess! {guess} is in the word.')
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")    
-
+ask_for_input()                  
 
